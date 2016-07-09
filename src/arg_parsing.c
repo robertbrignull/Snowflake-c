8d9d9c3418b4bb09b4e9767cbd42822ccf02f1a8
Robert Brignull <robert.brignull@gmail.com>
2016-07-09 13:58:45 +0100

    convert num_particles in to a named argument

--- src/arg_parsing.c
+++ src/arg_parsing.c
@@ -5,7 +5,10 @@
 #include "arg_parsing.h"
 
 void print_usage() {
-    printf("Usage: snowflake num_particles\n\n");
+    printf("Usage: snowflake (--num-particles 100000) [--output out/output.tga]\n\n");
+    printf("Required argumens:\n");
+    printf("  -n --num_particles    The number of particles to simulate\n\n");
+    printf("Optional argumens:\n");
     printf("  -o --output   Output filename\n");
     printf("                Default is out/output.tga\n\n");
     exit(0);
@@ -16,47 +19,56 @@ void print_missing_argument(char *arg) {
     print_usage();
 }
 
-void print_not_enough_paramters(char *arg, int num_params_expected, int num_params_found) {
-    printf("Argument %s expected %d parameters but found %d\n\n",
-        arg, num_params_expected, num_params_found);
-    print_usage();
-}
-
 void print_unrecognised_argument(char *arg) {
     printf("Unrecognised argument %s\n\n", arg);
     print_usage();
 }
 
+void check_enough_parameters(char *arg, int argc, int argi, int num_params_expected) {
+    if (argi + 1 >= argc) {
+        printf("Argument %s expected %d parameters but found %d\n\n",
+            arg, num_params_expected, argc - argi - 1);
+        print_usage();
+    }
+}
+
+int arg_matches(char *actual_arg, char *long_arg, char *short_arg) {
+    return (strcmp(long_arg, actual_arg) == 0 ||
+            strcmp(short_arg, actual_arg) == 0);
+}
+
 arg_options parse_args(int argc, char **argv) {
-    if (argc == 1 ||
-        strcmp("--help", argv[1]) == 0 || strcmp("-h", argv[1]) == 0)
-    {
+    if (argc >= 2 && arg_matches(argv[1], "--help", "-h")) {
         print_usage();
     }
 
-    if (argc < 2) {
-        print_missing_argument("num_particles");
-    }
+    // required args
+    int num_particles = 0;
+    int num_particles_set = 0;
 
-    int num_particles;
-    sscanf(argv[1], "%d", &num_particles);
-
+    // optional args
     char *output = "out/output.tga";
 
-    int argi = 2;
+    int argi = 1;
     while (argi < argc) {
-        if (strcmp("--output", argv[argi]) == 0 ||
-            strcmp("-o", argv[argi]) == 0)
-        {
-            if (argi + 1 >= argc) {
-                print_not_enough_paramters(argv[argi], 1, argc - argi - 1);
-            }
+        if (arg_matches(argv[argi], "--num-particles", "-n")) {
+            check_enough_parameters(argv[argi], argc, argi, 1);
+            num_particles = atoi(argv[argi+1]);
+            num_particles_set = 1;
+            argi += 2;
+        }
+        else if (arg_matches(argv[argi], "--output", "-o")) {
+            check_enough_parameters(argv[argi], argc, argi, 1);
             output = argv[argi+1];
             argi += 2;
         }
         else {
             print_unrecognised_argument(argv[argi]);
         }
+    }
+
+    if (!num_particles_set) {
+        print_missing_argument("--num-particles");
     }
 
     arg_options args;
