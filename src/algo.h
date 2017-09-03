8ce6b05a67e9d2d7aa9c01371a92991c03dec8e3
Robert Brignull <robertbrignull@gmail.com>
2017-09-03 18:33:49 +0100

    move single algo functions to header

--- src/algo.h
+++ src/algo.h
@@ -1,15 +1,25 @@
 #pragma once
 
+#include <math.h>
+
 #ifndef M_PI
 #    define M_PI 3.14159265358979323846
 #endif
 
 #define CHECK_MEM(m) if (m == 0) { printf("Memory allocation failure.\n"); exit(1); }
 
-int min(int x, int y);
+inline int max(int x, int y) {
+    return (x > y) ? x : y;
+}
 
-int max(int x, int y);
+inline int min(int x, int y) {
+    return (x < y) ? x : y;
+}
 
-double dist(double x, double y, double u, double v);
+inline double dist(double x, double y, double u, double v) {
+    return sqrt((x-u)*(x-u) + (y-v)*(y-v));
+}
 
-double dist_origin(double x, double y);
+inline double dist_origin(double x, double y) {
+    return sqrt(x*x + y*y);
+}
